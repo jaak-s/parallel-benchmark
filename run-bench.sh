ac943594fea4ad282f9dd506020528be628f86d2
Jaak Simm <jaak.simm@gmail.com>
2015-06-28 22:22:02 +0200

    run-bench options for output dir, nrepeats

--- run-bench.sh
+++ run-bench.sh
@@ -5,7 +5,6 @@
 ## run only dot
 ## ./run-bench.sh -b dot
 
-chip=E5-2699-v3
 cpu_min=1
 cpu_max=16
 system_cores=`grep -c ^processor /proc/cpuinfo`
@@ -14,32 +13,29 @@ if [[ $cpu_max -gt $system_cores ]]; then
 fi
 
 n=5000000
+nrepeats=1
 mintime=4 ## 4 seconds
 benchmarks="sum dot normsq"
-framework=openmp
-no_output=1
+framework=""
+output_dir=""
 
-while getopts ":b:f:o" opt; do
+usage() { echo "Usage: $0 -f openmp|cilk [-b dot|normsq|sum] [-o ouput_dir] [-r num_repeats]" 1>&2; exit 1; }
+
+while getopts ":b:f:o:r:" opt; do
   case $opt in
-    b)
-      benchmarks="$OPTARG"
-      ;;
-    f)
-      framework="$OPTARG"
-      ;;
-    o)
-      no_output=0
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
+    b) benchmarks="$OPTARG" ;;
+    f) framework="$OPTARG"  ;;
+    o) output_dir="$OPTARG" ;;
+    r) nrepeats="$OPTARG"   ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; usage;;
+    :)  echo "Option -$OPTARG requires an argument." >&2; usage;;
+    *)  usage;;
   esac
 done
+
+if [ -z "$framework" ]; then
+  echo "Please choose parallel framework with '-f openmp' or '-f cilk'" >&2; usage
+fi
 
 if [[ "$framework" == "openmp" ]]; then
   NTHREAD_VAR=OMP_NUM_THREADS
@@ -52,18 +48,24 @@ fi
 
 for bench in $benchmarks
 do
-  if [[ $no_output -eq 0 ]]; then
-    file=results/$chip/${bench}.${framework}.txt
-    echo "Running '$bench' with $framework, results saved to $file:"
+  if [ -n "$output_dir" ]; then
+    file=$output_dir/${bench}.${framework}.txt
+    echo "Running '$bench' with $framework, results will be saved to '$file':"
+    > $file
   else
     file=""
     echo "Running '$bench' with $framework, not saving results:"
   fi
-  for i in `seq $cpu_min $cpu_max`; do
-    echo -n -e "$i\t"
-    ## setting number of threads:
-    declare -x $NTHREAD_VAR=$i
-    $framework/$bench $n $mintime
-  done | tee $file
+
+  for repeat in `seq 1 $nrepeats`; do
+    echo "Repeat $repeat / $nrepeats"
+    for i in `seq $cpu_min $cpu_max`; do
+      echo -n -e "$i\t"
+      ## setting number of threads:
+      declare -x $NTHREAD_VAR=$i
+      $framework/$bench $n $mintime
+    done | tee -a $file
+  done
+
 done
 
